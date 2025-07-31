0822791053bf0a2142ee256be47e37a890d7d7b3
bogdanGR <fant1k450@gmail.com>
2025-07-31 20:11:41 +0300

    add base components and apply them

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index.js';
 import App from  './App.vue';
+import BaseCard from './components/ui/baseCard.vue';
+import BaseButton from './components/ui/baseButton.vue';
+import BaseBadge from './components/ui/baseBadge.vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard); // registering of a global component
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
