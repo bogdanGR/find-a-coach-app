a9eb30ca14f54752dc737ce30edc87b9f752369d
bogdanGR <fant1k450@gmail.com>
2025-09-16 19:08:53 +0300

    Add store of requests

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from 'vuex';
 import coachesModules from './modules/coaches/index.js';
-
+import requestsModules from './modules/requests/index.js';
 const store = createStore({
   modules: {
     coaches: coachesModules,
+    requests: requestsModules,
   },
   state() {
     return {
