5cd5bf983b214b95105eb8de0e95ca9c4182a734
bogdanGR <fant1k450@gmail.com>
2025-09-16 19:23:41 +0300

    render all requests on its page

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,50 @@
 <template>
-  REQUESTS
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+        <ul v-if="hasRequests">
+          <request-item v-for="request in receivedRequests"
+                        :email="request.userEmail"
+                        :message="request.message"
+                        :key="request.id"></request-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet!</h3>
+      </header>
+    </base-card>
+  </section>
 </template>
+<script>
+import requestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  components: {
+    requestItem
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+  }
+
+};
+</script>
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
