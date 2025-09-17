5bb5e765aa80ea7593f9aa3d3fddedf2f8878aae
bogdanGR <fant1k450@gmail.com>
2025-09-17 14:30:25 +0300

    Store and show requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,9 +1,13 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
-        <ul v-if="hasRequests">
+        <base-spinner v-if="isLoading"></base-spinner>
+        <ul v-else-if="hasRequests && !isLoading">
           <request-item v-for="request in receivedRequests"
                         :email="request.userEmail"
                         :message="request.message"
@@ -21,12 +25,36 @@ export default {
   components: {
     requestItem
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    }
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    }
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/getRequests');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 
