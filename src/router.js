1b21768a60009b88d1d324600ea409195c120360
bogdanGR <fant1k450@gmail.com>
2025-09-19 15:51:39 +0300

    don't use async comp in routes as it isn't recommended

--- src/router.js
+++ src/router.js
@@ -1,16 +1,14 @@
-import { defineAsyncComponent } from 'vue';
-
 import { createRouter, createWebHistory } from  'vue-router';
 
 import CoachesList from './pages/coaches/CoachesList.vue';
 import NotFound from './pages/notFound.vue';
 import store from './store/index.js';
 
-const CoachDetail = defineAsyncComponent(() => import('./pages/coaches/CoachDetail.vue'));
-const CoachRegistration = defineAsyncComponent(() => import('./pages/coaches/CoachRegistration.vue'));
-const ContactCoach = defineAsyncComponent(() => import( './pages/requests/ContactCoach.vue'));
-const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'));
-const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'));
+const CoachDetail =() => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import( './pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
