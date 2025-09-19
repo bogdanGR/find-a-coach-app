07337c8d060d3dab8d8a329704ff79f41b11ee98
bogdanGR <fant1k450@gmail.com>
2025-09-19 13:54:09 +0300

    add token to outgoing requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -22,7 +22,8 @@ export default {
   },
   async getRequests(context) {
     const coachId = context.rootGetters.userId;
-    const response = await fetch(`https://coachapp-418b6-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`);
+    const token = context.rootGetters.token;
+    const response = await fetch(`https://coachapp-418b6-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=${token}`);
     const responseData = await response.json();
 
     if (!response.ok) {
